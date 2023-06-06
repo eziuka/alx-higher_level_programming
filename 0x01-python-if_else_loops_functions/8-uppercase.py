4f85e34d3a82feb1dbe6002fbbdc3bd54c3058ff
eziuka <eziukajustice@gmail.com>
2023-06-06 08:29:58 -0700

    8. To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newstr = ""
+    for c in str:
+        c = ord(c)
+
+        if c > 96 and c < 123:
+            newstr += chr(c - 32)
+        else:
+            newstr += chr(c)
+    print("{}".format(newstr))
