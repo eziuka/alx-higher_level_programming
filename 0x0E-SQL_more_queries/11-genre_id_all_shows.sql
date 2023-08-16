d95d6144f71bc48db8215083280031e670eef56b
eziuka <eziukajustice@gmail.com>
2023-08-16 06:38:26 -0700

    11. Genre ID for all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- This Lists all shows contained in the database hbtn_0d_tvshows.
+-- it should Display NULL for shows without genres.
+-- The Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
